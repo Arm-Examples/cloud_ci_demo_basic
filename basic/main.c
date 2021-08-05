07f7b967582cebd8196e36ff63777e7e441485d7
Reinhard Keil <ReinhardKeil@users.noreply.github.com>
2021-08-05 16:02:56 +0200

    Revert change in main.c

--- basic/main.c
+++ basic/main.c
@@ -13,7 +13,7 @@ int my_sum(int a, int b) {
 // In test_my_sum.c - 
 int main(int argc, char *argv[]) {
   printf("Basic CI example\n");
-  assert(1 == my_sum(1, 1));
+  assert(2 == my_sum(1, 1));
   assert(-2 == my_sum(-1, -1));
   assert(0 == my_sum(0, 0));
   return(0);
